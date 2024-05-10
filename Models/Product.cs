f154ec47311db7518de484cc1ca2bee937bdccb5
Erick <erick.mauricio30@hotmail.com>
2024-05-09 23:51:00 -0500

    Message

---
+++ Models/Product.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SupermarketWEB.Models
+{
+    public class Product
+    {
+        // [Key] -> Anotación si la propiedad no se llama Id, ejemplo ProductId
+        public int Id { get; set; } //Será la llave primaria
+        public string Name { get; set; }
+
+        [Column(TypeName = "decimal(6.2)")]
+        public decimal Price { get; set; }
+        public int Stock {  get; set; }
+        public int CategoruId { get; set; }//Será la llave foranea
+        public Category Category { get; set; } // Propiedad de navegación 
+
+    }
+}
